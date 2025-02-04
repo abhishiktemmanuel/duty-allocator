a2cc07bde2f5669a8066298bac5aa2a34edc3b8d
Abhishikt <deepikaabhiprakash02@gmail.com>
2025-02-04 14:15:38 +0530

    feat: update Vite config to load backend URL from environment variables

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import process from "process";
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": "http://localhost:4000",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    server: {
+      proxy: {
+        "/api": env.VITE_BACKEND_URL,
+      },
     },
-  },
-  plugins: [react(), tailwindcss()],
-  optimizeDeps: {
-    include: ["jwt-decode"],
-  },
+    plugins: [react(), tailwindcss()],
+    optimizeDeps: {
+      include: ["jwt-decode"],
+    },
+  };
 });
