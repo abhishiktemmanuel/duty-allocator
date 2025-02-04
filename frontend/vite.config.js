6516f16d324804228d18ec75fae0d4cb376b8dff
Abhishikt <deepikaabhiprakash02@gmail.com>
2025-02-04 19:55:28 +0530

    feat: update Vite config to use a fixed backend API URL for proxying requests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,16 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import process from "process";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  return {
-    server: {
-      proxy: {
-        "/api": env.VITE_BACKEND_URL,
-      },
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "https://duty-allocator.onrender.com",
     },
-    plugins: [react(), tailwindcss()],
-    optimizeDeps: {
-      include: ["jwt-decode"],
-    },
-  };
+  },
+  plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ["jwt-decode"],
+  },
 });
