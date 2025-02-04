6b49460663e5c49011879a72a2ba1f7308a6640f
Abhishikt <deepikaabhiprakash02@gmail.com>
2025-02-04 22:47:32 +0530

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import process from "node:process";
 
-const API_URL = import.meta.env.VITE_API_URL;
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": API_URL,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+  return {
+    server: {
+      proxy: {
+        "/api": env.VITE_API_URL,
+      },
     },
-  },
-  plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss()],
+  };
 });
